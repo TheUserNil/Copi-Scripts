3cac2022816f173534233e9b1be6c9827d070afa
VidalGonzalesZyx <102625768+VidalGonzalesZyx@users.noreply.github.com>
2022-04-10 22:19:25 -0500

    Update FeChipsDessertPlate.lua

--- FeChipsDessertPlate.lua
+++ FeChipsDessertPlate.lua
@@ -186,9 +186,10 @@ char['Right Leg']:Destroy()
 char['Left Leg']:Destroy()
 char.Torso['Left Shoulder']:Destroy()
 char['Left Arm']:BreakJoints()
-fix(char['Left Arm'])
-Pos(char['Left Arm'],reanim.DessertPlate.Handle)
 
+Pos(char['Left Arm'],reanim.DessertPlate.Handle)
+fix(char['Left Arm'])
+flinger(char['Left Arm'])
 te(ct,m.Button1Down:Connect(function()
 --click=true
 end))
@@ -208,7 +209,6 @@ end
 end))
 
 
-flinger(char['Left Arm'])
 
 
 chatted=false
